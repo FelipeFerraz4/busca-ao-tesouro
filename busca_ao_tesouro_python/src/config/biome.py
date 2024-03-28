1bbb8e28c442de68055a9fa71a2e4ab9d76df925
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-03-28 19:01:37 -0300

    create biome image

--- busca_ao_tesouro_python/src/config/biome.py
+++ busca_ao_tesouro_python/src/config/biome.py
@@ -1,4 +1,5 @@
 import pygame
+from src.graph.search import depthFirstSearch
 
 class Biome():
     def __init__(self, x, y, image):
@@ -8,13 +9,39 @@ class Biome():
         
     def draw_biome(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
-        
+
 def create_biome():
-    images = []
+    biome = pygame.image.load('./src/assets/biome-img/safe_biome.jpeg')
+    biome = pygame.transform.scale(biome, (200, 200))
+    return Biome(800, 0, biome)
+   
+def create_biome_0():
     biome = pygame.image.load('./src/assets/image/home_beach_biome.jpeg')
     biome = pygame.transform.scale(biome, (200, 200))
     return Biome(800, 0, biome)
 
+def create_biome_1():
+    biome = pygame.image.load('./src/assets/biome-img/carnivorus_plant_biome.jpg')
+    biome = pygame.transform.scale(biome, (200, 200))
+    return Biome(800, 0, biome)
 
-def draw_biomes(biome, surface, type):
-    biome.draw_biome(surface)
+def create_biome_2():
+    biome = pygame.image.load('./src/assets/biome-img/healing_plant_biome1.jpeg')
+    biome = pygame.transform.scale(biome, (200, 200))
+    return Biome(800, 0, biome)
+
+def create_biome_3():
+    biome = pygame.image.load('./src/assets/biome-img/poisonous_plant_biome.jpg')
+    biome = pygame.transform.scale(biome, (200, 200))
+    return Biome(800, 0, biome)
+
+def create_biome_4():
+    biome = pygame.image.load('./src/assets/biome-img/rock_slide_biome.jpeg')
+    biome = pygame.transform.scale(biome, (200, 200))
+    return Biome(800, 0, biome)
+
+def create_biome_5():
+    biome = pygame.image.load('./src/assets/biome-img/swamp_biome.jpg')
+    biome = pygame.transform.scale(biome, (200, 200))
+    return Biome(800, 0, biome)
+
