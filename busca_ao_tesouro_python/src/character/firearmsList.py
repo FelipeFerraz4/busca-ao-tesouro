21b23a97e767bca184930a515920ffb31a2c7762
Ivna <ivnacontato.dev@gmail.com>
2024-03-29 14:19:35 -0300

    adding character lists

---
+++ busca_ao_tesouro_python/src/character/firearmsList.py
@@ -0,0 +1,26 @@
+firearms =[
+    {
+        "name": "Pistol",
+        "attack_points": 10,
+        "remaining-uses": 3
+    },
+
+    {
+        "name": "Rifle",
+        "attack_points": 20,
+        "remaining-uses": 3
+    },
+
+    {
+        "name": "Shotgun",
+        "attack_points": 40,
+        "remaining-uses": 3
+    },
+
+    {
+        "name": "Machine gun",
+        "attack_points": 40,
+        "remaining-uses": 3
+    },
+
+]
---
+++ busca_ao_tesouro_python/src/character/monstersList.py
@@ -0,0 +1,34 @@
+monsters = [
+    
+    {
+        'type': 'hawk',
+        'coordinates': (100, 150), 
+        'adjacent_vertices': [1, 2, 3],  
+        'health_points': 100,  
+        'attack_points': 20 
+    },
+    
+    {
+        'type': 'formiga',
+        'coordinates': (200, 250), 
+        'adjacent_vertices': [4, 5],  
+        'health_points': 100,
+        'attack_points': 10  
+    },
+
+    {
+        'type': 'onca',
+        'coordinates': (300, 350), 
+        'adjacent_vertices': [6, 7, 8],  
+        'health_points': 100,
+        'attack_points': 30  
+    },
+
+    {
+        'type': 'jacare',
+        'coordinates': (400, 450), 
+        'adjacent_vertices': [9, 10],  
+        'health_points': 100,
+        'attack_points': 30  
+    }
+]
