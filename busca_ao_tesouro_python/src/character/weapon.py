f287ffd4c326f7a3a08095ea96c0694f18db7132
Ivna <ivnacontato.dev@gmail.com>
2024-03-30 17:32:05 -0300

    updating weapon

--- busca_ao_tesouro_python/src/character/weapon.py
+++ busca_ao_tesouro_python/src/character/weapon.py
@@ -1,5 +1,13 @@
 class Weapon:
-    def __init__(self, attack_bonus, treasure_penalty):
+    def __init__(self, attack_bonus, treasure_penalty, remaining_usage ):
         self.attack_bonus = attack_bonus 
         self.treasure_penalty = treasure_penalty  
+        self.remaining_usage = remaining_usage
 
+def use_weapon(self):
+        
+        if self.usos_restantes > 0:
+            self.usos_restantes -= 1
+            return self.usos_restantes
+        else:
+            return 0
